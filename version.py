0d2c73b91ba8196f6669f273a90b9f4f6cdb2c45
Dave Glover <dglover@microsoft.com>
2019-04-03 02:48:57 +1100

    update to version helper

--- version.py
+++ version.py
@@ -5,10 +5,12 @@ from pprint import pprint
 import os.path
 from os.path import isdir
 from os import listdir
+from packaging import version
 # from os.path import *
 modulesFileBase = "modules/"
 
-def updateModule(moduleName, version):
+
+def updateModule(moduleName, versionX):
     filename = modulesFileBase + moduleName + "/module.json"
     if not os.path.exists(filename):
         return False
@@ -16,44 +18,29 @@ def updateModule(moduleName, version):
     with open(filename) as f:
         data = json.load(f)
 
-    data["image"]["tag"]["version"] = version
+    # todo increment micro release
+    # major.minor.micro
+    # https://www.python.org/dev/peps/pep-0440/
+
+    v = data["image"]["tag"]["version"]
+
+    ver =  tuple(map(int, (v.split("."))))
+    micro = ver[2:3][0]
+    micro = micro + 1
+    new_ver = f"{ver[0:1][0]}.{ver[1:2][0]}.{micro}"
+
+    data["image"]["tag"]["version"] = new_ver
 
     with open(filename, 'w') as outfile:
         json.dump(data, outfile, indent=4)
 
     return True
 
-def updateDeployment(moduleName, version):
-    print(moduleName)
-    with open('deployment.template.json') as f:
-        data = json.load(f)
-
-    try:
-        v = data["moduleContent"]["$edgeAgent"]["properties.desired"]["modules"][moduleName]["settings"]["image"]
-    except:
-        return
-
-    baseIdx = v.find(":")
-    suffixIdx = v.find("-")
-
-    base = v[0:baseIdx]
-    suffix = v[suffixIdx:None]
-
-    newImage = base + ":" + version + suffix
-
-    data["moduleContent"]["$edgeAgent"]["properties.desired"]["modules"][moduleName]["settings"]["image"] = newImage
-
-    with open('deployment.template.json', 'w') as outfile:
-        json.dump(data, outfile, indent=4)
-
-    print(moduleName + " updated")
-
-
 
 def updateVersion(moduleName):
     print(moduleName)
     version = "0.1." + str(random.randint(1, 10000))
-   
+
     if not updateModule(moduleName, version):
         return
 
